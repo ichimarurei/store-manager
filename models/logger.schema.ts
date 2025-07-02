539ffb303e4c88a8a0d4b8fb2e221221311c2b0c
Muhammad Iqbal <jms21maru@gmail.com>
2025-07-02 07:05:40 +0700

    sync logger

--- models/logger.schema.ts
+++ models/logger.schema.ts
@@ -1,11 +1,13 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema, Types } from 'mongoose';
 
 const Logger: Schema = new Schema({
-    date: { type: Date, required: false, default: new Date() },
-    log: { type: Object, required: true }
+    key: { type: Types.ObjectId, unique: true, required: true },
+    log: { type: Object, required: true },
+    date: { type: Date, required: false, default: new Date() }
 });
 
 export interface LoggerDocument extends Document {
+    key: string;
     log: any;
 }
 
